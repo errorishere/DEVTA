0294bfaca3870102d1492302f41de992a551402f
E990R <63518845+errorishere@users.noreply.github.com>
2021-08-26 18:37:29 +0530

    Add files via upload

---
+++ DATA.sh
@@ -0,0 +1,4 @@
+F="Found"
+N="\nError 7 | Please Contact With Dev's\nError 7 | Please Contact With Dev's\nError 7 | Please Contact With Dev's\nError 7 | Please Contact With Dev's\nError 7 | Please Contact With Dev's\nError 7 | Please Contact With Dev's"
+U="UPDATED"
+PATH1="/storage/emulated/0/T_O_X_I_C/Check_UPDATE"
