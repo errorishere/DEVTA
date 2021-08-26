9a47787679b83df5d0befda6a9d1eb0f9dc7f058
E990R <63518845+errorishere@users.noreply.github.com>
2021-08-26 18:57:38 +0530

    Delete SD.sh

--- SD.sh
+++
@@ -1,9 +0,0 @@
-SD="/data/media/0/T_O_X_I_C.zip"
-DN="Files Not Found\nDownloading Files\nIt May Takes 3-4 Minutes\nDepends On Internet Speed"
-MD="/storage/emulated/0/T_O_X_I_C/DATA.sh"
-DC="\n\nDownload Complete\nRestart DEVTA.sh"
-STOR="/storage/emulated/0/"
-
-
-
-
