4833555b437818504b988d5669ad6ffec8d0c179
E990R <63518845+errorishere@users.noreply.github.com>
2021-08-26 18:51:04 +0530

    Update SD.sh

--- SD.sh
+++ SD.sh
@@ -2,7 +2,8 @@ SD="/data/media/0/T_O_X_I_C.zip"
 DN="Files Not Found\nDownloading Files\nIt May Takes 3-4 Minutes\nDepends On Internet Speed"
 MD="/storage/emulated/0/T_O_X_I_C/DATA.sh"
 DC="\n\nDownload Complete\nRestart DEVTA.sh"
-
+DD="/storage/emulated/0/OX-T_O_X_I_C"
+CD="/storage/emulated/0/T_O_X_I_C"
 
 
 
