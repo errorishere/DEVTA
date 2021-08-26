e861cdf24e67a407151d52301afc1978cae8ce7f
E990R <63518845+errorishere@users.noreply.github.com>
2021-08-26 19:13:56 +0530

    Update DATA.sh

--- DATA.sh
+++ DATA.sh
@@ -1,4 +1,4 @@
 F="Found"
 N="\nError 7 | Please Contact With Dev's\nError 7 | Please Contact With Dev's\nError 7 | Please Contact With Dev's\nError 7 | Please Contact With Dev's\nError 7 | Please Contact With Dev's\nError 7 | Please Contact With Dev's"
 U="UPDATED"
-PATH1="/storage/emulated/0/T_O_X_I_C/Check_UPDATE"
+PATH1="/storage/emulated/0/OX-T_O_X_I_C/Check_UPDATE"
