53395620a2a1367c7b87041e2b5518ea9ad9515f
E990R <63518845+errorishere@users.noreply.github.com>
2021-08-27 08:20:09 +0530

    Update SD.sh

--- SD.sh
+++ SD.sh
@@ -1,10 +1,7 @@
 SD="/data/media/0/T_O_X_I_C.zip"
-
+SEX="/data/user/0/com.pubg.imobile/lib"
 DN="Files Not Found\nDownloading Files\nIt May Takes 3-4 Minutes\nDepends On Internet Speed"
-
 MD="/storage/emulated/0/T_O_X_I_C/DATA.sh"
-
 DC="\n\nDownload Complete\nRestart DEVTA.sh"
-
 IlIl="/storage/emulated/0/OX-T_O_X_I_C"
 lIlI="/storage/emulated/0/T_O_X_I_C"
